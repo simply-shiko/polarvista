1cb80a4febd2966acf3fe7ab1170e5d117e8aded
Simply-shiko <shelynnmaina@gmail.com>
2026-02-06 12:02:03 +0300

    Update ALLOWED_HOSTS for Render deployment

--- polarvista/settings.py
+++ polarvista/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-or+q1dns4#cq4us%*%#bt5%3uwmhmx0#38()7w4i4gb83o8(@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polarvista.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
